49065dbd95045bdce8b62ff1d4d137b8b7f05c65
Antti Saloranta <saloranta.antti@gmail.com>
2018-03-23 14:59:46 +0200

    Added player function to audioRecorder

--- audioRecorder.py
+++ audioRecorder.py
@@ -36,3 +36,27 @@ def nauhoitus(pituus, tiedosto, polku):
     waveFile.setframerate(RATE)
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
+
+def toisto(tiedosto, polku):
+    CHUNK = 1024
+    filename = polku + "/" + tiedosto + ".wav"
+
+    wf = wave.open(filename, 'rb')
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
